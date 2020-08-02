41d9f9eb5966569c813e252862e8991895b1143c
Clovis de Villars <clovis.devillars@gmail.com>
2020-08-03 01:10:14 +0200

    Replace strings by same size random string for anonymize

--- anonymizer.py
+++ anonymizer.py
@@ -1,22 +1,23 @@
-from random import randrange, choice
+import string
+from random import randrange, choice, choices
 
 
-def anonymize(data):
+def anonymize(data, empty_string=False):
     data_type = type(data)
     if data_type is list:
         new_data = []
         for item in data:
-            new_data.append(anonymize(item))
+            new_data.append(anonymize(item, empty_string=empty_string))
         return new_data
     if data_type is dict:
         new_data = {}
         for item in data.items():
-            new_data[item[0]] = anonymize(item[1])
+            new_data[item[0]] = anonymize(item[1], empty_string=empty_string)
         return new_data
     if data_type is int:
         return anonymize_int(data)
     if data_type is str:
-        return anonymize_str(data)
+        return anonymize_str(data, empty_string)
     if data_type is bool:
         return anonymize_bool(data)
     return data
@@ -27,8 +28,10 @@ def anonymize_int(data: int) -> int:
     return randrange(10 ** (length - 1), 10 ** length - 1)
 
 
-def anonymize_str(data: str) -> str:
-    return ""
+def anonymize_str(data: str, empty_string=False) -> str:
+    if empty_string:
+        return ''
+    return ''.join(choices(string.ascii_letters + string.digits, k=len(data)))
 
 
 def anonymize_bool(data: bool) -> bool:
